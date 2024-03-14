5cc6ea6343450cd2d3277f673b3489cedd39e07d
miri-j-kim <miri.kim@ucalgary.ca>
2024-03-13 20:28:46 -0600

    Step C

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,12 @@
+// src/components/ProductList.js
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../services/apiService';
 import { useProductsContext } from '../context/ProductsContext';
+import { useNavigate } from 'react-router-dom';
 
 const ProductList = () => {
   const { products, setProducts } = useProductsContext();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getProducts = async () => {
@@ -13,10 +16,21 @@ const ProductList = () => {
     getProducts();
   }, [setProducts]);
 
+  const handleProductDeleted = (deletedProductId) => {
+    setProducts(currentProducts => currentProducts.filter (product => product.id !== deletedProductId ) ) ;
+    alert ('Product deleted successfully !');
+    };
+
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>{product.title}</div>
+    <div className="list-group">
+      {products.map((product, index) => (
+        <button
+          key={product.id}
+          className="list-group-item d-flex justify-content-between align-items-center hover-highlight"
+          onClick={({handleProductDeleted}) => navigate(`/product/${product.id}`)}
+        >
+          <span>{index + 1}. {product.title}</span>
+        </button>
       ))}
     </div>
   );
